a1bdc6247d28f64dc40a9a6c3ac741f32e740685
SidRajAlok <100489532+SidRajAlok@users.noreply.github.com>
2022-04-12 11:44:25 +0530

    Update signup.sql

--- signup.sql
+++ signup.sql
@@ -4,26 +4,3 @@ CREATE TABLE `signup` (
   `UN` varchar(256) NOT NULL,
   `PS` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `signup`
---
-
-INSERT INTO `signup` (`UN`, `PS`) VALUES
-('Dishti', 'DO123@456'),
-('DO', 'Dishti123@');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `signup`
---
-ALTER TABLE `signup`
-  ADD UNIQUE KEY `UN` (`UN`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
