beaedc9469941b2d8dcb691dd9b7d1a0865188da
SidRajAlok <100489532+SidRajAlok@users.noreply.github.com>
2022-04-12 10:55:54 +0530

    Add files via upload

---
+++ bookings.sql
@@ -0,0 +1,2 @@
+use database_1;
+CREATE TABLE `database_1`.`passengers` ( `Names` VARCHAR(100) NOT NULL , `Airpno` VARCHAR(20) NOT NULL , `DDate` VARCHAR(20) NOT NULL , `phone` VARCHAR(20) NOT NULL , `email` VARCHAR(20) NOT NULL , `class` VARCHAR(20) NOT NULL ) ENGINE = InnoDB;
